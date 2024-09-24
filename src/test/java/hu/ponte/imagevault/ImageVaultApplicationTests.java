d8632be19485227d6675ee9dba95f6edcbe8f9c6
David Suemegi <david.suemegi.osv@fedex.com>
2024-09-24 17:27:20 +0200

    initial commit

---
+++ src/test/java/hu/ponte/imagevault/ImageVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.ponte.imagevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
